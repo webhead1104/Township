d9a9be8eafe9805b0b8a980455353c147597a5f3
Webhead1104 <webhead1104@hotmail.com>
2024-12-08 11:42:15 -0600

    Fix italic names

--- src/main/java/me/webhead1104/township/utils/Msg.java
+++ src/main/java/me/webhead1104/township/utils/Msg.java
@@ -1,16 +1,17 @@
 package me.webhead1104.township.utils;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.json.JSONComponentSerializer;
 
 public class Msg {
 
     public static Component format(String miniMessageString) {
-        return MiniMessage.miniMessage().deserialize(miniMessageString);
+        return MiniMessage.miniMessage().deserialize(miniMessageString).decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE);
     }
 
     public static String formatToJson(String miniMessageString) {
-        return JSONComponentSerializer.json().serialize(MiniMessage.miniMessage().deserialize(miniMessageString));
+        return JSONComponentSerializer.json().serialize(format(miniMessageString));
     }
 }
