ca0658969b617e6d012e8b3247ea210831344104
Webhead1104 <webhead1104@hotmail.com>
2025-03-12 20:49:31 -0600

    Remove gson
    Signed-off-by: Webhead1104 <webhead1104@hotmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,6 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("org.xerial:sqlite-jdbc:3.47.2.0")
-    compileOnly("com.google.code.gson:gson:2.11.0")
     compileOnly("com.zaxxer:HikariCP:6.2.1")
     implementation("dev.velix:imperat-core:1.5.1")
     implementation("dev.velix:imperat-bukkit:1.5.1")
