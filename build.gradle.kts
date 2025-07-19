7f91813278e8a4d1ac48ea3b40b9a87dd582a7e7
webhead1104 <webhead1104@hotmail.com>
2025-07-18 19:04:01 -0500

    Use a generated class loader

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,61 @@ tasks.withType<Javadoc> {
     javadocOptions.addStringOption("source", "21")
 }
 
+val generateClassloader = tasks.register("generateClassloader") {
+
+    val outputDir = file("$projectDir/build/generated/sources/classloader")
+    val packageDir = File(outputDir, "me/webhead1104/webhead1104/township/utils")
+    val classloaderFile = File(packageDir, "GeneratedClassloader.java")
+
+    doLast {
+        packageDir.mkdirs()
+        outputDir.mkdirs()
+        classloaderFile.createNewFile()
+
+        val deps = configurations.getByName("compileOnly")
+            .dependencies
+            .filterIsInstance<ModuleDependency>()
+            .filter { it.group != "io.papermc.paper" && it.group != "net.strokkur" }
+            .map { "${it.group}:${it.name}:${it.version}" }
+
+        classloaderFile.writeText(
+            """
+            package me.webhead1104.township.utils;
+            
+            import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+            import io.papermc.paper.plugin.loader.PluginLoader;
+            import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
+            import org.eclipse.aether.artifact.DefaultArtifact;
+            import org.eclipse.aether.graph.Dependency;
+            import org.eclipse.aether.repository.RemoteRepository;
+            import org.jetbrains.annotations.NotNull;
+
+            public class GeneratedClassloader implements PluginLoader {
+
+                @Override
+                public void classloader(@NotNull PluginClasspathBuilder classpathBuilder) {
+                    System.setProperty("bstats.relocatecheck", "false");
+                    MavenLibraryResolver resolver = new MavenLibraryResolver();
+
+                    resolver.addRepository(new RemoteRepository.Builder("central", "default", MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR).build());
+                    
+                    ${deps.joinToString("\n                    ") { "resolver.addDependency(new Dependency(new DefaultArtifact(\"$it\"), null));" }}
+
+                    classpathBuilder.addLibrary(resolver);
+                }
+            }
+            """.trimIndent()
+        )
+
+        println("Generated GeneratedClassloader.java at $classloaderFile")
+    }
+}
+
+tasks.compileJava {
+    dependsOn(generateClassloader)
+    source(generateClassloader.map { layout.buildDirectory.dir("generated/sources/classloader").get() })
+}
+
 tasks {
     assemble {
         dependsOn(shadowJar)
@@ -73,7 +128,7 @@ tasks {
         apiVersion.set("1.21")
         author.set("Webhead1104")
         description.set("A remake of the game Township by Playrix in Minecraft")
-        loader.set("me.webhead1104.township.TownshipLoader")
+        loader.set("me.webhead1104.township.utils.GeneratedClassloader")
         website.set("https://github.com/Webhead1104/Township")
     }
 }
