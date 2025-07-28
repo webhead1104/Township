45fbc07d0939b21cb697307570dcb031035caf31
webhead1104 <webhead1104@hotmail.com>
2025-07-28 10:45:04 -0500

    Remove WorldTIleType from animal and factory types

--- src/main/java/me/webhead1104/township/data/enums/AnimalType.java
+++ src/main/java/me/webhead1104/township/data/enums/AnimalType.java
@@ -11,17 +11,16 @@ import org.bukkit.inventory.ItemStack;
 @Getter
 @AllArgsConstructor
 public enum AnimalType {
-    COWSHED_1(Msg.format("<gold>Cowshed"), cowItemStack(), ItemType.COW_FEED, ItemType.MILK, WorldTileType.COWSHED_1, 3, 0, 1, 0, 10, "cowshed_1"),
-    COWSHED_2(Msg.format("<gold>Cowshed"), cowItemStack(), ItemType.COW_FEED, ItemType.MILK, WorldTileType.COWSHED_2, 3, 1000, 15, 46, 10, "cowshed_2"),
-    COWSHED_3(Msg.format("<gold>Cowshed"), cowItemStack(), ItemType.COW_FEED, ItemType.MILK, WorldTileType.COWSHED_3, 3, 5000, 22, 211, 10, "cowshed_3"),
-    CHICKEN_COOP_1(Msg.format("<gold>Chicken Coop"), chickenItemStack(), ItemType.CHICKEN_FEED, ItemType.EGG, WorldTileType.CHICKEN_COOP_1, 4, 200, 5, 13, 3600, "chicken_coop_1"),
-    CHICKEN_COOP_2(Msg.format("<gold>Chicken Coop"), chickenItemStack(), ItemType.CHICKEN_FEED, ItemType.EGG, WorldTileType.CHICKEN_COOP_2, 4, 3000, 24, 128, 3600, "chicken_coop_2"),
-    CHICKEN_COOP_3(Msg.format("<gold>Chicken Coop"), chickenItemStack(), ItemType.CHICKEN_FEED, ItemType.EGG, WorldTileType.CHICKEN_COOP_3, 4, 15000, 39, 623, 3600, "chicken_coop_3");
+    COWSHED_1(Msg.format("<gold>Cowshed"), cowItemStack(), ItemType.COW_FEED, ItemType.MILK, 3, 0, 1, 0, 10, "cowshed_1"),
+    COWSHED_2(Msg.format("<gold>Cowshed"), cowItemStack(), ItemType.COW_FEED, ItemType.MILK, 3, 1000, 15, 46, 10, "cowshed_2"),
+    COWSHED_3(Msg.format("<gold>Cowshed"), cowItemStack(), ItemType.COW_FEED, ItemType.MILK, 3, 5000, 22, 211, 10, "cowshed_3"),
+    CHICKEN_COOP_1(Msg.format("<gold>Chicken Coop"), chickenItemStack(), ItemType.CHICKEN_FEED, ItemType.EGG, 4, 200, 5, 13, 3600, "chicken_coop_1"),
+    CHICKEN_COOP_2(Msg.format("<gold>Chicken Coop"), chickenItemStack(), ItemType.CHICKEN_FEED, ItemType.EGG, 4, 3000, 24, 128, 3600, "chicken_coop_2"),
+    CHICKEN_COOP_3(Msg.format("<gold>Chicken Coop"), chickenItemStack(), ItemType.CHICKEN_FEED, ItemType.EGG, 4, 15000, 39, 623, 3600, "chicken_coop_3");
     private final Component menuTitle;
     private final ItemStack animalItemStack;
     private final ItemType feedType;
     private final ItemType productType;
-    private final WorldTileType tileType;
     private final int xpGivenOnClaim;
     private final int coinsNeeded;
     private final int levelNeeded;
--- src/main/java/me/webhead1104/township/data/enums/FactoryType.java
+++ src/main/java/me/webhead1104/township/data/enums/FactoryType.java
@@ -10,12 +10,12 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public enum FactoryType {
-    BAKERY(Msg.format("<gold>Bakery"), "bakery", List.of(RecipeType.BREAD, RecipeType.COOKIE, RecipeType.BAGEL), 2, 55, 125, 10, WorldTileType.BAKERY),
-    FEED_MILL_1(Msg.format("<gold>Feed Mill"), "feed_mill_1", List.of(RecipeType.COW_FEED, RecipeType.CHICKEN_FEED), 3, 60, 150, 11, WorldTileType.FEED_MILL_1),
-    FEED_MILL_2(Msg.format("<gold>Feed Mill"), "feed_mill_2", List.of(RecipeType.COW_FEED, RecipeType.CHICKEN_FEED), 19, 655, 2800, 120, WorldTileType.FEED_MILL_2),
-    FEED_MILL_3(Msg.format("<gold>Feed Mill"), "feed_mill_3", List.of(RecipeType.COW_FEED, RecipeType.CHICKEN_FEED), 44, 4150, 40000, 1655, WorldTileType.FEED_MILL_3),
-    DAIRY_FACTORY(Msg.format("<gold>Dairy Factory"), "dairy_factory", List.of(RecipeType.CREAM, RecipeType.CHEESE), 4, 65, 175, 12, WorldTileType.DAIRY_FACTORY),
-    SUGAR_FACTORY(Msg.format("<gold>Sugar Factory"), "sugar_factory", List.of(RecipeType.SUGAR), 7, 75, 250, 15, WorldTileType.SUGAR_FACTORY);
+    BAKERY(Msg.format("<gold>Bakery"), "bakery", List.of(RecipeType.BREAD, RecipeType.COOKIE, RecipeType.BAGEL), 2, 55, 125, 10),
+    FEED_MILL_1(Msg.format("<gold>Feed Mill"), "feed_mill_1", List.of(RecipeType.COW_FEED, RecipeType.CHICKEN_FEED), 3, 60, 150, 11),
+    FEED_MILL_2(Msg.format("<gold>Feed Mill"), "feed_mill_2", List.of(RecipeType.COW_FEED, RecipeType.CHICKEN_FEED), 19, 655, 2800, 120),
+    FEED_MILL_3(Msg.format("<gold>Feed Mill"), "feed_mill_3", List.of(RecipeType.COW_FEED, RecipeType.CHICKEN_FEED), 44, 4150, 40000, 1655),
+    DAIRY_FACTORY(Msg.format("<gold>Dairy Factory"), "dairy_factory", List.of(RecipeType.CREAM, RecipeType.CHEESE), 4, 65, 175, 12),
+    SUGAR_FACTORY(Msg.format("<gold>Sugar Factory"), "sugar_factory", List.of(RecipeType.SUGAR), 7, 75, 250, 15);
     private final Component menuTitle;
     private final String ID;
     private final List<RecipeType> recipes;
@@ -23,5 +23,4 @@ public enum FactoryType {
     private final int populationNeeded;
     private final int coinsNeeded;
     private final int xpGiven;
-    private final WorldTileType tileType;
 }
