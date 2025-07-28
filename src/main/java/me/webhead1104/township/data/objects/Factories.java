b5e60ab88204eca9529bb7c0c2454edccfb7830a
webhead1104 <webhead1104@hotmail.com>
2025-07-28 11:07:38 -0500

    Remove NoArgsConstructor import

--- src/main/java/me/webhead1104/township/data/objects/Factories.java
+++ src/main/java/me/webhead1104/township/data/objects/Factories.java
@@ -1,7 +1,6 @@
 package me.webhead1104.township.data.objects;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import me.webhead1104.township.data.enums.FactoryType;
 import me.webhead1104.township.data.enums.ItemType;
