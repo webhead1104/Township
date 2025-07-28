e600b3b105b8090321b63e3ca6960bb9a7c02c6b
webhead1104 <webhead1104@hotmail.com>
2025-07-28 12:27:32 -0500

    New world menu

--- src/main/java/me/webhead1104/township/managers/WorldManager.java
+++ src/main/java/me/webhead1104/township/managers/WorldManager.java
@@ -1,163 +1,52 @@
 package me.webhead1104.township.managers;
 
 import me.webhead1104.township.Township;
-import me.webhead1104.township.data.enums.WorldTileType;
-import me.webhead1104.township.data.objects.Expansion;
-import me.webhead1104.township.data.objects.Plot;
-import me.webhead1104.township.data.objects.User;
-import me.webhead1104.township.data.objects.World;
-import me.webhead1104.township.utils.*;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
+import me.webhead1104.township.menus.WorldMenu;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-
-import java.util.List;
-import java.util.Objects;
 
 public class WorldManager {
-
-    public Inventory getWorld(Player player, int section) {
-        player.setItemOnCursor(ItemStack.empty());
-        User user = Township.getUserManager().getUser(player.getUniqueId());
-        user.setSection(section);
-        World world = user.getWorld();
-        Inventory inventory = Bukkit.createInventory(null, 54, Msg.format("World Menu"));
-        world.getSection(section).getSlotMap().forEach((key, value) -> {
-            ItemBuilder builder = new ItemBuilder(value.getTileType().getItem());
-            if (value.getPlot() != null) {
-                Plot plot = value.getPlot();
-                builder.pdcSetString(Keys.plotDataKey, plot.toString());
-                builder.id(plot.getPlotType().getId());
-                builder.material(plot.getPlotType().getMenuItem().getType());
-                builder.displayName(new ItemBuilder(plot.getPlotType().getMenuItem()).getDisplayName());
-            }
-            if (value.getExpansion() != null) {
-                Expansion expansion = value.getExpansion();
-                builder.pdcSetString(Keys.expansionDataKey, expansion.toString());
-                builder.id("expansion");
-                builder.material(Material.PODZOL);
-                builder.displayName(Msg.format("Expansion"));
-                builder.lore(List.of(Msg.format("<aqua>Click to open the expansion menu!")));
-            }
-            if (value.getTileType().equals(WorldTileType.TRAIN) && !user.getTrains().isUnlocked()) {
-                builder.lore(List.of(Msg.format("<red>You need to purchase the trains!")));
-            }
-            if (value.getTileType().getAnimalType() != null && !user.getAnimals().getAnimalBuilding(value.getTileType().getAnimalType()).isUnlocked()) {
-                builder.lore(List.of(Msg.format("<red>You need to purchase the " + Utils.thing2(value.getTileType().getId()) + "!")));
-            }
-            inventory.setItem(key, builder.build());
-        });
-        return inventory;
-    }
-
-    public Inventory getWorld(Player player) {
-        return getWorld(player, Township.getUserManager().getUser(player.getUniqueId()).getSection());
-    }
-
-    public void openWorldMenu(Player player, int section) {
-        Utils.openInventory(player, getWorld(player, section), uuid -> openConfirmCloseMenu(player), null);
-        User user = Township.getUserManager().getUser(player.getUniqueId());
-        player.getInventory().clear();
-        ItemStack arrowUp = new ItemBuilder(MenuItems.arrow)
-                .material(Material.ARROW)
-                .id("world_arrow")
-                .displayName(Msg.format("<dark_green>Click to scroll up!"))
-                .pdcSetInt(Keys.newPageKey, section - 8)
-                .build();
-        ItemStack arrowRight = new ItemBuilder(MenuItems.arrow)
-                .material(Material.ARROW)
-                .id("world_arrow")
-                .displayName(Msg.format("<dark_green>Click to scroll right!"))
-                .pdcSetInt(Keys.newPageKey, section + 1)
-                .build();
-        ItemStack arrowDown = new ItemBuilder(MenuItems.arrow)
-                .material(Material.ARROW)
-                .id("world_arrow")
-                .displayName(Msg.format("<dark_green>Click to scroll up!"))
-                .pdcSetInt(Keys.newPageKey, section + 8)
-                .build();
-        ItemStack arrowLeft = new ItemBuilder(MenuItems.arrow)
-                .material(Material.ARROW)
-                .id("world_arrow")
-                .displayName(Msg.format("<dark_green>Click to scroll left!"))
-                .pdcSetInt(Keys.newPageKey, section - 1)
-                .build();
-        switch (section) {
-            case 0 -> {
-                player.getInventory().setItem(23, arrowRight);
-                player.getInventory().setItem(31, arrowDown);
-            }
-            case 1, 2, 3, 4, 5, 6 -> {
-                player.getInventory().setItem(21, arrowLeft);
-                player.getInventory().setItem(23, arrowRight);
-                player.getInventory().setItem(31, arrowDown);
-            }
-            case 7 -> {
-                player.getInventory().setItem(21, arrowLeft);
-                player.getInventory().setItem(31, arrowDown);
-            }
-            case 15, 23, 31, 39, 47, 55 -> {
-                player.getInventory().setItem(21, arrowLeft);
-                player.getInventory().setItem(31, arrowDown);
-                player.getInventory().setItem(13, arrowUp);
-            }
-            case 63 -> {
-                player.getInventory().setItem(13, arrowUp);
-                player.getInventory().setItem(21, arrowLeft);
-            }
-            case 62, 61, 60, 59, 58, 57 -> {
-                player.getInventory().setItem(21, arrowLeft);
-                player.getInventory().setItem(23, arrowRight);
-                player.getInventory().setItem(13, arrowUp);
-            }
-            case 8, 16, 24, 32, 40, 48 -> {
-                player.getInventory().setItem(23, arrowRight);
-                player.getInventory().setItem(13, arrowUp);
-                player.getInventory().setItem(31, arrowDown);
-            }
-            case 56 -> {
-                player.getInventory().setItem(13, arrowUp);
-                player.getInventory().setItem(23, arrowRight);
-            }
-            default -> {
-                player.getInventory().setItem(21, arrowLeft);
-                player.getInventory().setItem(23, arrowRight);
-                player.getInventory().setItem(13, arrowUp);
-                player.getInventory().setItem(31, arrowDown);
-            }
-        }
-        ItemBuilder profile = new ItemBuilder(MenuItems.profile).material(Material.LIGHT_BLUE_CONCRETE).displayName(Msg.format("<green>" + user.getTownName()));
-        player.getInventory().setItem(22, profile.build());
-        ItemBuilder levelAndPop = new ItemBuilder(MenuItems.levelAndPop);
-        levelAndPop.displayName(Msg.format("<aqua>Level " + user.getLevel().getLevel()));
-        if (Township.getLevelManager().getLevelMap().containsKey(user.getLevel().getLevel() + 1)) {
-            levelAndPop.lore(List.of(Msg.format("<aqua>Xp " + user.getLevel().getXp()),
-                    Msg.format(user.getLevel().getProgressBar()), Msg.format("<red>Population " + user.getPopulation())));
-        } else {
-            levelAndPop.lore(List.of(Msg.format("<dark_red>You have reached the max level!"),
-                    Msg.format("<red>Population " + user.getPopulation())));
-        }
-        player.getInventory().setItem(9, levelAndPop.build());
-        ItemBuilder coinsAndCash = new ItemBuilder(MenuItems.coinsAndCash)
-                .displayName(Msg.format("<yellow>Coins " + user.getCoins()))
-                .lore(List.of(Msg.format("<green>Cash " + user.getCash())));
-        player.getInventory().setItem(17, coinsAndCash.build());
-    }
 
     public void openWorldMenu(Player player) {
         openWorldMenu(player, Township.getUserManager().getUser(player.getUniqueId()).getSection());
     }
 
-    public void openConfirmCloseMenu(Player player) {
-        player.getInventory().clear();
-        Inventory inventory = Bukkit.createInventory(null, 9, Msg.format("<red>Are you sure?"));
-        ItemBuilder confirm = new ItemBuilder(Material.RED_CONCRETE, Msg.format("<red>Are you sure you want to close township?"), "confirm_close");
-        confirm.lore(List.of(Msg.format("<red>If so click this item!"),
-                Msg.format("<green>Or if don't want to close township hit the Esc key or click the back button!")));
-        inventory.setItem(4, confirm.build());
-        inventory.setItem(8, MenuItems.backButton);
-        Utils.openInventory(player, inventory, uuid -> openWorldMenu(Objects.requireNonNull(Bukkit.getPlayer(uuid))), null);
+    public void openWorldMenu(Player player, int section) {
+        Township.getViewFrame().open(WorldMenu.class, player, section);
+    }
+
+    /**
+     * @param slotIndex 0‑based index [0..53], row‑major order
+     * @return true if there is a slot immediately to the right
+     */
+    public boolean canMoveRight(int slotIndex) {
+        int x = slotIndex % 8;
+        return x < 8 - 1;
+    }
+
+    /**
+     * @param slotIndex 0‑based index [0..53], row‑major order
+     * @return true if there is a slot immediately to the left
+     */
+    public boolean canMoveLeft(int slotIndex) {
+        int x = slotIndex % 8;
+        return x > 0;
+    }
+
+    /**
+     * @param slotIndex 0‑based index [0..53], row‑major order
+     * @return true if there is a slot immediately below
+     */
+    public boolean canMoveDown(int slotIndex) {
+        int y = slotIndex / 8;
+        return y < 8 - 1;
+    }
+
+    /**
+     * @param slotIndex 0‑based index [0..53], row‑major order
+     * @return true if there is a slot immediately above
+     */
+    public boolean canMoveUp(int slotIndex) {
+        int y = slotIndex / 8;
+        return y > 0;
     }
 }
--- src/main/java/me/webhead1104/township/menus/WorldMenu.java
+++ src/main/java/me/webhead1104/township/menus/WorldMenu.java
@@ -1,21 +1,20 @@
 package me.webhead1104.township.menus;
 
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.ItemLore;
 import me.devnatan.inventoryframework.View;
 import me.devnatan.inventoryframework.ViewConfigBuilder;
 import me.devnatan.inventoryframework.context.CloseContext;
 import me.devnatan.inventoryframework.context.OpenContext;
 import me.devnatan.inventoryframework.context.RenderContext;
+import me.devnatan.inventoryframework.context.SlotClickContext;
 import me.devnatan.inventoryframework.state.MutableState;
+import me.devnatan.inventoryframework.state.State;
 import me.webhead1104.township.Township;
-import me.webhead1104.township.data.enums.WorldTileType;
-import me.webhead1104.township.data.objects.Expansion;
-import me.webhead1104.township.data.objects.Plot;
 import me.webhead1104.township.data.objects.User;
 import me.webhead1104.township.data.objects.World;
-import me.webhead1104.township.utils.ItemBuilder;
-import me.webhead1104.township.utils.Keys;
 import me.webhead1104.township.utils.Msg;
-import me.webhead1104.township.utils.Utils;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -24,26 +23,30 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public class WorldMenu extends View {
-    private final MutableState<Integer> sectionState = initialState();
+    private final State<Integer> sectionState = initialState();
+    private final MutableState<Boolean> openConfirmClose = mutableState(true);
 
     @Override
     public void onInit(@NotNull ViewConfigBuilder config) {
         config.scheduleUpdate(20);
         config.cancelInteractions();
         config.size(6);
+        config.title(Msg.format("World Menu"));
     }
 
     @Override
     public void onOpen(@NotNull OpenContext context) {
-        context.modifyConfig().title("World Menu");
-        Player player = context.getPlayer();
-        player.getInventory().clear();
-        player.setItemOnCursor(ItemStack.empty());
+        context.getPlayer().getInventory().clear();
+        context.getPlayer().setItemOnCursor(ItemStack.empty());
     }
 
     @Override
     public void onClose(@NotNull CloseContext context) {
-        Township.getWorldManager().openConfirmCloseMenu(context.getPlayer());
+        Bukkit.getScheduler().runTaskLater(Township.getInstance(), () -> {
+            if (openConfirmClose.get(context)) {
+                Township.getViewFrame().open(ConfirmCloseMenu.class, context.getPlayer());
+            }
+        }, 1);
     }
 
     @Override
@@ -51,30 +54,66 @@ public class WorldMenu extends View {
         Player player = context.getPlayer();
         User user = Township.getUserManager().getUser(player.getUniqueId());
         World world = user.getWorld();
-        world.getSection(sectionState.get(context)).getSlotMap().forEach((key, value) -> {
-            ItemBuilder builder = new ItemBuilder(value.getTileType().getItem());
-            if (value.getPlot() != null) {
-                Plot plot = value.getPlot();
-                builder.pdcSetString(Keys.plotDataKey, plot.toString());
-                builder.id(plot.getPlotType().getId());
-                builder.material(plot.getPlotType().getMenuItem().getType());
-                builder.displayName(new ItemBuilder(plot.getPlotType().getMenuItem()).getDisplayName());
+        world.getSection(sectionState.get(context)).getSlotMap().forEach((key, tile) -> context.slot(key).onRender(slotRenderContext -> slotRenderContext.setItem(tile.render(slotRenderContext))).onClick(tile::onClick));
+
+        ItemStack rightItemStack = ItemStack.of(Material.ARROW);
+        rightItemStack.setData(DataComponentTypes.ITEM_NAME, Msg.format("<dark_green>Click to scroll right!"));
+        player.getInventory().setItem(23, rightItemStack);
+
+        ItemStack downItemStack = ItemStack.of(Material.ARROW);
+        downItemStack.setData(DataComponentTypes.ITEM_NAME, Msg.format("<dark_green>Click to scroll down!"));
+        player.getInventory().setItem(31, downItemStack);
+
+        ItemStack leftItemStack = ItemStack.of(Material.ARROW);
+        leftItemStack.setData(DataComponentTypes.ITEM_NAME, Msg.format("<dark_green>Click to scroll left!"));
+        player.getInventory().setItem(21, leftItemStack);
+
+        ItemStack upItemStack = ItemStack.of(Material.ARROW);
+        upItemStack.setData(DataComponentTypes.ITEM_NAME, Msg.format("<dark_green>Click to scroll up!"));
+        player.getInventory().setItem(13, upItemStack);
+
+        ItemStack profileItemStack = ItemStack.of(Material.LIGHT_BLUE_CONCRETE);
+        profileItemStack.setData(DataComponentTypes.ITEM_NAME, Msg.format("<green>%s", user.getTownName()));
+        player.getInventory().setItem(22, profileItemStack);
+
+        ItemStack levelAndPopStack = ItemStack.of(Material.BLUE_CONCRETE);
+        levelAndPopStack.setData(DataComponentTypes.ITEM_NAME, Msg.format("<aqua>Level %d", user.getLevel().getLevel()));
+        if (Township.getLevelManager().getLevelMap().containsKey(user.getLevel().getLevel() + 1)) {
+            levelAndPopStack.setData(DataComponentTypes.LORE, ItemLore.lore(List.of(
+                    Msg.format("<aqua>Xp %d", user.getLevel().getXp()),
+                    Msg.format(user.getLevel().getProgressBar()),
+                    Msg.format("<red>Population %d", user.getPopulation())
+            )));
+        } else {
+            levelAndPopStack.setData(DataComponentTypes.LORE, ItemLore.lore(List.of(
+                    Msg.format("<dark_red>You have reached the max level!"),
+                    Msg.format("<red>Population " + user.getPopulation())
+            )));
+        }
+        player.getInventory().setItem(9, levelAndPopStack);
+
+        ItemStack coinsAndCashStack = ItemStack.of(Material.GOLD_BLOCK);
+        coinsAndCashStack.setData(DataComponentTypes.ITEM_NAME, Msg.format("<yellow>Coins %d", user.getCoins()));
+        coinsAndCashStack.setData(DataComponentTypes.LORE, ItemLore.lore(List.of(Msg.format("<green>Cash %d", user.getCash()))));
+        player.getInventory().setItem(17, coinsAndCashStack);
+    }
+
+    @Override
+    public void onClick(@NotNull SlotClickContext context) {
+        if (context.isOnEntityContainer()) {
+            if (context.getClickedSlot() == 68) {
+                context.openForPlayer(WorldMenu.class, sectionState.get(context) + 1);
+                openConfirmClose.set(false, context);
+            } else if (context.getClickedSlot() == 76) {
+                context.openForPlayer(WorldMenu.class, sectionState.get(context) + 8);
+                openConfirmClose.set(false, context);
+            } else if (context.getClickedSlot() == 66) {
+                context.openForPlayer(WorldMenu.class, sectionState.get(context) - 1);
+                openConfirmClose.set(false, context);
+            } else if (context.getClickedSlot() == 58) {
+                context.openForPlayer(WorldMenu.class, sectionState.get(context) - 8);
+                openConfirmClose.set(false, context);
             }
-            if (value.getExpansion() != null) {
-                Expansion expansion = value.getExpansion();
-                builder.pdcSetString(Keys.expansionDataKey, expansion.toString());
-                builder.id("expansion");
-                builder.material(Material.PODZOL);
-                builder.displayName(Msg.format("Expansion"));
-                builder.lore(List.of(Msg.format("<aqua>Click to open the expansion menu!")));
-            }
-            if (value.getTileType().equals(WorldTileType.TRAIN) && !user.getTrains().isUnlocked()) {
-                builder.lore(List.of(Msg.format("<red>You need to purchase the trains!")));
-            }
-            if (value.getTileType().getAnimalType() != null && !user.getAnimals().getAnimalBuilding(value.getTileType().getAnimalType()).isUnlocked()) {
-                builder.lore(List.of(Msg.format("<red>You need to purchase the " + Utils.thing2(value.getTileType().getId()) + "!")));
-            }
-            context.slot(key, builder.build());
-        });
+        }
     }
 }
