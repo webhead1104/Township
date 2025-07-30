c4faae99054226a340b89ff87b328cad1322be16
webhead1104 <webhead1104@hotmail.com>
2025-07-30 16:21:30 -0500

    Add time data to PlotType

--- src/main/java/me/webhead1104/township/data/enums/PlotType.java
+++ src/main/java/me/webhead1104/township/data/enums/PlotType.java
@@ -1,22 +1,55 @@
 package me.webhead1104.township.data.enums;
 
-import lombok.AllArgsConstructor;
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.ItemLore;
 import lombok.Getter;
-import me.webhead1104.township.utils.MenuItems;
+import me.webhead1104.township.utils.Msg;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.List;
+
 @Getter
-@AllArgsConstructor
 public enum PlotType {
-    NONE(ItemType.NONE, MenuItems.World.plot, -1, -1, -1, "none_plot"),
-    WHEAT(ItemType.WHEAT, MenuItems.wheat, 0, 1, 1, "wheat_plot"),
-    CORN(ItemType.CORN, MenuItems.corn, 1, 3, 1, "corn_plot"),
-    CARROT(ItemType.CARROT, MenuItems.carrot, 2, 4, 2, "carrot_plot"),
-    SUGARCANE(ItemType.SUGARCANE, MenuItems.sugarcane, 3, 7, 3, "sugarcane_plot");
+    NONE(ItemType.NONE, getPlotItemStack(), -1, -1, -1, -1),
+    WHEAT(ItemType.WHEAT, 0, 1, 1, 10), //120
+    CORN(ItemType.CORN, 1, 3, 1, 300),
+    CARROT(ItemType.CARROT, 2, 4, 2, 600),
+    SUGARCANE(ItemType.SUGARCANE, 3, 7, 3, 1200);
     private final ItemType itemType;
+    @Getter(value = lombok.AccessLevel.NONE)
     private final ItemStack menuItem;
     private final int price;
     private final int levelNeeded;
     private final int xpGiven;
-    private final String id;
+    private final int time;
+
+    PlotType(ItemType itemType, ItemStack menuItem, int price, int levelNeeded, int xpGiven, int time) {
+        this.itemType = itemType;
+        this.menuItem = menuItem;
+        this.price = price;
+        this.levelNeeded = levelNeeded;
+        this.xpGiven = xpGiven;
+        this.time = time;
+    }
+
+    PlotType(ItemType itemType, int price, int levelNeeded, int xpGiven, int time) {
+        this.itemType = itemType;
+        this.menuItem = itemType.getItemStack();
+        this.price = price;
+        this.levelNeeded = levelNeeded;
+        this.xpGiven = xpGiven;
+        this.time = time;
+    }
+
+    private static ItemStack getPlotItemStack() {
+        ItemStack itemStack = ItemStack.of(Material.FARMLAND);
+        itemStack.setData(DataComponentTypes.ITEM_NAME, Msg.format("Empty Plot"));
+        itemStack.setData(DataComponentTypes.LORE, ItemLore.lore(List.of(Msg.format("<green>You should plant something!"))));
+        return itemStack;
+    }
+
+    public ItemStack getMenuItem() {
+        return menuItem.clone();
+    }
 }
