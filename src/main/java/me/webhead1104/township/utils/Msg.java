29d33457a24c7ba361c056baf6a0cca3316209be
webhead1104 <webhead1104@hotmail.com>
2025-09-10 13:52:58 -0500

    Remove Msg.formatToJson

--- src/main/java/me/webhead1104/township/utils/Msg.java
+++ src/main/java/me/webhead1104/township/utils/Msg.java
@@ -3,15 +3,10 @@ package me.webhead1104.township.utils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.serializer.json.JSONComponentSerializer;
 
 public class Msg {
 
     public static Component format(String miniMessageString, Object... args) {
         return MiniMessage.miniMessage().deserialize(String.format(miniMessageString, args)).decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE);
-    }
-
-    public static String formatToJson(String miniMessageString) {
-        return JSONComponentSerializer.json().serialize(format(miniMessageString));
     }
 }
