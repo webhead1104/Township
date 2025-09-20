ef97ba3f6dd82061f4d230ddfac85666fc02504b
Webhead1104 <webhead1104@hotmail.com>
2025-09-20 16:34:55 -0500

    Remove buildingType from AnimalType
    Signed-off-by: Webhead1104 <webhead1104@hotmail.com>

--- src/main/java/me/webhead1104/township/features/animals/AnimalType.java
+++ src/main/java/me/webhead1104/township/features/animals/AnimalType.java
@@ -2,7 +2,6 @@ package me.webhead1104.township.features.animals;
 
 import lombok.Getter;
 import me.webhead1104.township.data.enums.ItemType;
-import me.webhead1104.township.features.world.build.BuildingType;
 import me.webhead1104.township.utils.Msg;
 import me.webhead1104.township.utils.Utils;
 import net.kyori.adventure.text.Component;
@@ -13,12 +12,12 @@ import java.time.Duration;
 
 @Getter
 public enum AnimalType {
-    COWSHED_1("Cowshed", Utils.getItemStack("Cow", Material.COW_SPAWN_EGG), ItemType.COW_FEED, ItemType.MILK, 3, Duration.ofSeconds(20), BuildingType.COWSHED),
-    COWSHED_2("Cowshed", Utils.getItemStack("Cow", Material.COW_SPAWN_EGG), ItemType.COW_FEED, ItemType.MILK, 3, Duration.ofMinutes(20), BuildingType.COWSHED),
-    COWSHED_3("Cowshed", Utils.getItemStack("Cow", Material.COW_SPAWN_EGG), ItemType.COW_FEED, ItemType.MILK, 3, Duration.ofMinutes(20), BuildingType.COWSHED),
-    CHICKEN_COOP_1("Chicken Coop", Utils.getItemStack("Chicken", Material.COW_SPAWN_EGG), ItemType.CHICKEN_FEED, ItemType.EGG, 4, Duration.ofHours(1), BuildingType.CHICKEN_COOP),
-    CHICKEN_COOP_2("Chicken Coop", Utils.getItemStack("Chicken", Material.COW_SPAWN_EGG), ItemType.CHICKEN_FEED, ItemType.EGG, 4, Duration.ofHours(1), BuildingType.CHICKEN_COOP),
-    CHICKEN_COOP_3("Chicken Coop", Utils.getItemStack("Chicken", Material.COW_SPAWN_EGG), ItemType.CHICKEN_FEED, ItemType.EGG, 4, Duration.ofHours(1), BuildingType.CHICKEN_COOP);
+    COWSHED_1("Cowshed", Utils.getItemStack("Cow", Material.COW_SPAWN_EGG), ItemType.COW_FEED, ItemType.MILK, 3, Duration.ofSeconds(20)),
+    COWSHED_2("Cowshed", Utils.getItemStack("Cow", Material.COW_SPAWN_EGG), ItemType.COW_FEED, ItemType.MILK, 3, Duration.ofMinutes(20)),
+    COWSHED_3("Cowshed", Utils.getItemStack("Cow", Material.COW_SPAWN_EGG), ItemType.COW_FEED, ItemType.MILK, 3, Duration.ofMinutes(20)),
+    CHICKEN_COOP_1("Chicken Coop", Utils.getItemStack("Chicken", Material.COW_SPAWN_EGG), ItemType.CHICKEN_FEED, ItemType.EGG, 4, Duration.ofHours(1)),
+    CHICKEN_COOP_2("Chicken Coop", Utils.getItemStack("Chicken", Material.COW_SPAWN_EGG), ItemType.CHICKEN_FEED, ItemType.EGG, 4, Duration.ofHours(1)),
+    CHICKEN_COOP_3("Chicken Coop", Utils.getItemStack("Chicken", Material.COW_SPAWN_EGG), ItemType.CHICKEN_FEED, ItemType.EGG, 4, Duration.ofHours(1));
     private final String name;
     private final Component menuTitle;
     @Getter(value = lombok.AccessLevel.NONE)
@@ -27,9 +26,8 @@ public enum AnimalType {
     private final ItemType productType;
     private final int xpGivenOnClaim;
     private final Duration timeTakesToFeed;
-    private final BuildingType buildingType;
 
-    AnimalType(String name, ItemStack animalItemStack, ItemType feedType, ItemType productType, int xpGivenOnClaim, Duration timeTakesToFeed, BuildingType buildingType) {
+    AnimalType(String name, ItemStack animalItemStack, ItemType feedType, ItemType productType, int xpGivenOnClaim, Duration timeTakesToFeed) {
         this.name = name;
         this.menuTitle = Msg.format("<gold>%s", name);
         this.animalItemStack = animalItemStack;
@@ -37,7 +35,6 @@ public enum AnimalType {
         this.productType = productType;
         this.xpGivenOnClaim = xpGivenOnClaim;
         this.timeTakesToFeed = timeTakesToFeed;
-        this.buildingType = buildingType;
     }
 
     public ItemStack getAnimalItemStack() {
