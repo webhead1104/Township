64cbcdd08b4d9c6a1eab15cbb6ed2e9bb485e9d7
Webhead1104 <webhead1104@hotmail.com>
2025-11-07 12:17:44 -0600

    Run join event async
    Signed-off-by: Webhead1104 <webhead1104@hotmail.com>

--- src/main/java/me/webhead1104/township/listeners/JoinListener.java
+++ src/main/java/me/webhead1104/township/listeners/JoinListener.java
@@ -4,6 +4,7 @@ import lombok.NoArgsConstructor;
 import me.webhead1104.township.Township;
 import me.webhead1104.township.data.objects.User;
 import me.webhead1104.township.database.UserLoader;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,20 +20,22 @@ public class JoinListener implements Listener {
         long start = System.currentTimeMillis();
         Player player = event.getPlayer();
         UserLoader userLoader = Township.getLoaderManager().getUserLoader();
-        try {
-            if (userLoader.userExists(player.getUniqueId())) {
-                User user = User.fromJson(userLoader.readUser(player.getUniqueId()));
-                Township.getUserManager().setUser(user.getUuid(), user);
-                Township.logger.info("player {} has joined. data has been loaded!", player.getName());
-            } else {
-                Township.logger.info("player {} has joined. data not found. creating new player...", player.getName());
-                User user = new User(player.getUniqueId());
-                userLoader.saveUser(player.getUniqueId(), user.toString());
-                Township.getUserManager().setUser(player.getUniqueId(), user);
+        Bukkit.getScheduler().runTaskAsynchronously(Township.getInstance(), () -> {
+            try {
+                if (userLoader.userExists(player.getUniqueId())) {
+                    User user = User.fromJson(userLoader.readUser(player.getUniqueId()));
+                    Township.getUserManager().setUser(user.getUuid(), user);
+                    Township.logger.info("player {} has joined. data has been loaded!", player.getName());
+                } else {
+                    Township.logger.info("player {} has joined. data not found. creating new player...", player.getName());
+                    User user = new User(player.getUniqueId());
+                    userLoader.saveUser(player.getUniqueId(), user.toString());
+                    Township.getUserManager().setUser(player.getUniqueId(), user);
+                }
+            } catch (IOException e) {
+                Township.logger.error("An error occurred whilst loading player data!", e);
             }
-        } catch (IOException e) {
-            Township.logger.error("An error occurred whilst loading player data!", e);
-        }
-        Township.logger.info("Join event done in {} mills", System.currentTimeMillis() - start);
+            Township.logger.info("Join event done in {} mills", System.currentTimeMillis() - start);
+        });
     }
 }
