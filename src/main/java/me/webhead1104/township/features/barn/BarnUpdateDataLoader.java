d79aa3ff141542fe654a386e320e1ae1c807e301
Webhead1104 <webhead1104@hotmail.com>
2025-11-07 11:35:52 -0600

    Generate barn upgrades at runtime
    Signed-off-by: Webhead1104 <webhead1104@hotmail.com>

--- src/main/java/me/webhead1104/township/features/barn/BarnUpdateDataLoader.java
+++ src/main/java/me/webhead1104/township/features/barn/BarnUpdateDataLoader.java
@@ -27,8 +27,25 @@ public class BarnUpdateDataLoader implements DataLoader.IntegerBasedDataLoader<B
     @Override
     public void load() {
         try {
-            List<BarnUpgrade> list = getListFromFile("/data/barnUpgrades.json", BarnUpgrade.class);
-            values.addAll(list);
+            int storage = 50;
+
+            for (int i = 1; i <= 100; i++) {
+                int increment;
+
+                if (i <= 3) {
+                    increment = 20;
+                } else if (i <= 39) {
+                    increment = 25;
+                } else if (i <= 59) {
+                    increment = 50;
+                } else {
+                    increment = 75;
+                }
+
+                storage += increment;
+                values.add(new BarnUpgrade(i, i, storage));
+            }
+
             Township.logger.info("Loaded {} barn upgrades!", values.size());
         } catch (Exception e) {
             throw new RuntimeException("An error occurred whilst loading barn upgrades! Please report the following stacktrace to Webhead1104:", e);
