5b9707b19a420454b7ce69e6d7bd89b0bfca3ad2
Webhead1104 <webhead1104@hotmail.com>
2025-11-11 14:28:47 -0600

    Fix mongodb depend version
    Signed-off-by: Webhead1104 <webhead1104@hotmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation("me.devnatan:inventory-framework-platform-paper:3.5.5")
     implementation("me.devnatan:inventory-framework-platform-bukkit:3.5.5")
     implementation("io.github.classgraph:classgraph:4.8.184")
-    compileOnly("org.mongodb:mongodb-driver-sync:5.7.0-SNAPSHOT")
+    compileOnly("org.mongodb:mongodb-driver-sync:5.7.0-alpha0")
     compileOnly("com.zaxxer:HikariCP:7.0.2")
 
     compileOnly("net.strokkur:commands-annotations:1.5.0")
