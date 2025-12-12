69a4aba3257cc304f39c90a9ca321229f893da72
Webhead1104 <webhead1104@hotmail.com>
2025-12-12 12:42:11 -0700

    Fix testing again
    Signed-off-by: Webhead1104 <webhead1104@hotmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,9 @@
 rootProject.name = "Towncraft"
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 
 include("tools:wiki-scraper", "tools:menu-designer")
 include(":platforms:common")
 include(":platforms:cytosis")
 include(":platforms:paper")
-
-//project(":platforms:common").name = "Towncraft-Common"
-//project(":platforms:cytosis").name = "Towncraft-Cytosis"
-//project(":platforms:paper").name = "Towncraft-Paper"
